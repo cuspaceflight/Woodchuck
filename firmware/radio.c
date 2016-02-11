ac33e142802fac1c75bfe53c6937dd16e066d2fd
Aniruddh Raghu <aniruddraghu@gmail.com>
2016-02-11 21:12:06 +0000

    Added Makefile and fixed radio bugs- now compiles

--- firmware/radio.c
+++ firmware/radio.c
@@ -16,8 +16,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <util/delay.h>
-#include <util/crc16.h>
+//#include <util/delay.h>
+//#include <util/crc16.h>
 
 // libopencm3 includes here
 #include <libopencm3/cm3/nvic.h>
@@ -26,6 +26,7 @@
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/dac.h>
 
 #include "led.h"
 #include "radio.h"
@@ -49,6 +50,10 @@
 #define STOP_BIT				1
 #define LOGICAL_ONE				100
 #define LOGICAL_ZERO			183
+
+static void radio_transmit_string(char * string);
+static uint16_t radio_calculate_checksum(char* data);
+
 
 /**
 * Take a channel and a value and send it to the radio.
@@ -86,7 +91,7 @@ static void clock_setup()
 	rcc_periph_clock_enable(RCC_USART1);
 
 	/* Enable clock for timer6, to be used for interrupts */
-	rcc_periph_clock_enable(RCC_TIM6);
+	rcc_periph_clock_enable(RCC_TIM2);
 
 	//Enable APB1 (Advanced Peripheral Bus) clock for DAC
 	rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_DACEN);
@@ -97,7 +102,6 @@ static void clock_setup()
 */
 static void gpio_setup()
 {
-
 	gpio_mode_setup(RADIO_EN_GPIO, GPIO_MODE_AF, GPIO_PUPD_NONE, RADIO_EN_PIN);
 
 	/* Setup USART1 TX pin as alternate function. */
@@ -112,7 +116,7 @@ static void usart_setup()
 {
 	usart_set_baudrate(RADIO_USART, RADIO_BAUD_RATE);
 	usart_set_databits(RADIO_USART, RADIO_NUM_DATA_BITS);
-	usart_set_stopbits(RADIO_USART, USART_STOPBITS_1);
+	usart_set_stopbits(RADIO_USART, USART_CR2_STOP_1_0BIT);
 	usart_set_mode(RADIO_USART, USART_MODE_TX);
 	usart_set_parity(RADIO_USART, USART_PARITY_NONE);
 	usart_set_flow_control(RADIO_USART, USART_FLOWCONTROL_NONE);
@@ -125,8 +129,7 @@ static void usart_setup()
 */
 static void dac_setup()
 {
-	gpio_set_mode(RADIO_TX_GPIO, GPIO_MODE_OUTPUT_50_MHZ,
-	              GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, RADIO_TX_DATA);
+	gpio_mode_setup(RADIO_TX_GPIO, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, RADIO_TX_DATA);
 	dac_disable(CHANNEL_1);
 	dac_set_waveform_characteristics(DAC_CR_MAMP1_8);
 	dac_set_waveform_generation(DAC_CR_WAVE1_TRI);
@@ -137,44 +140,44 @@ static void dac_setup()
 }
 
 /**
-* Set up TIM6 for radio interrupts
+* Set up TIM2 for radio interrupts
 * TODO Check that we don't need to start the timer immediately
 */
-static void setup_tim6()
+static void setup_TIM2()
 {
-	timer_reset(TIM6);
+	timer_reset(TIM2);
 	/* 48MHz / 12kHz -1. */
-	timer_set_prescaler(TIM6, 3999); /* 48MHz/12kHz - 1 */
+	timer_set_prescaler(TIM2, 3999); /* 48MHz/12kHz - 1 */
 	/* 12kHz for 1 tick = 12 khz overflow */
-	timer_set_period(TIM6, 1);
-	timer_continuous_mode(TIM6);
+	timer_set_period(TIM2, 1);
+	timer_continuous_mode(TIM2);
 }
 
 /**
-* Enable interrupts on TIM6 and start it.
+* Enable interrupts on TIM2 and start it.
 * TODO Check that we don't need to reset the timer
 */
-static void start_tim6()
+static void start_TIM2()
 {
-	timer_disable_counter(TIM6);
-	timer_clear_flag(TIM6, TIM_SR_UIF);
-	nvic_enable_irq(NVIC_TIM6_IRQ);
-	timer_enable_update_event(TIM6); /* Unclear */
-	timer_enable_irq(TIM6, TIM_DIER_UIE);
-	timer_set_counter(TIM6, 0);
-	timer_enable_counter(TIM6);
+	timer_disable_counter(TIM2);
+	timer_clear_flag(TIM2, TIM_SR_UIF);
+	nvic_enable_irq(NVIC_TIM2_IRQ);
+	timer_enable_update_event(TIM2); /* Unclear */
+	timer_enable_irq(TIM2, TIM_DIER_UIE);
+	timer_set_counter(TIM2, 0);
+	timer_enable_counter(TIM2);
 }
 
 /**
-* Disable interrupts on TIM6 and stop it
+* Disable interrupts on TIM2 and stop it
 * TODO Check that we don't need to reset the timer
 */
-static void stop_tim6()
+static void stop_TIM2()
 {
-	timer_clear_flag(TIM6, TIM_SR_UIF);
-	nvic_disable_irq(NVIC_TIM6_IRQ);
-	timer_disable_counter(TIM6);
-	timer_set_counter(TIM6, 0);
+	timer_clear_flag(TIM2, TIM_SR_UIF);
+	nvic_disable_irq(NVIC_TIM2_IRQ);
+	timer_disable_counter(TIM2);
+	timer_set_counter(TIM2, 0);
 }
 
 /**
@@ -185,7 +188,7 @@ void radio_init()
 	clock_setup();
 	gpio_setup();
 	usart_setup();
-	setup_tim6();
+	setup_TIM2();
 	dac_setup();
 	//TODO add other initialisation stuff here, like setting
 	//the operating frequency, channel number, and TX power.
@@ -199,10 +202,10 @@ void radio_init()
  */
 void radio_transmit_sentence(char * message)
 {
-	radio_transmit_string(string);
+	radio_transmit_string(message);
     
     // Calculate the checksum and send it
-    uint16_t checksum = radio_calculate_checksum(string);
+    uint16_t checksum = radio_calculate_checksum(message);
     char cs[7];
     sprintf(cs, "*%04X\n", checksum);
     radio_transmit_string(cs);
@@ -221,7 +224,7 @@ static void radio_transmit_string(char* string)
         _txptr = 0;
         byte_complete = false;
         // Enable interrupts
-        start_tim6();
+        start_TIM2();
         while(!byte_complete)
         	;
         iwdg_reset();
@@ -285,11 +288,11 @@ static void _radio_transmit_bit(uint8_t data, uint8_t ptr)
 
 
 /**
- * Interrupt service routine for TIM6 (Radio)
+ * Interrupt service routine for TIM2 (Radio)
  * Every time we get here, continue process of transmitting
  * a bit of data to the radio.
  */
-void tim6_isr()
+void tim2_isr()
 {
 	if( systicks < 1 )
     {
@@ -297,7 +300,7 @@ void tim6_isr()
     }
     else
     {
-        if( _txptr < 11 )
+        if( _txptr < 11 ) //Should this be 10?
         {
             _radio_transmit_bit(_txbyte, _txptr);
             _txptr++;
@@ -305,7 +308,7 @@ void tim6_isr()
         else 
         {
             //Clear the timer
-            stop_tim6();
+            stop_TIM2();
             byte_complete = true;
         }
         systicks = 0;
