8bc8d637fcb96023a5d5e8e416ffbf849d5352cd
Gregory Brooks <gregox273@gmail.com>
2017-05-14 20:05:26 +0100

    Complete first radio driver attempt

--- firmware/radio.c
+++ firmware/radio.c
@@ -17,19 +17,6 @@
 
 #include <stdio.h>
 #include <string.h>
-//#include <util/delay.h>
-//#include <util/crc16.h>
-
-/* libopencm3 includes here
-#include <libopencm3/cm3/nvic.h>
-#include <libopencm3/stm32/f0/nvic.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/exti.h>
-#include <libopencm3/stm32/timer.h>
-#include <libopencm3/stm32/usart.h>
-#include <libopencm3/stm32/dac.h>
-*/
 
 #include "led.h"
 #include "radio.h"
@@ -50,15 +37,7 @@
 #define RADIO_BAUD_RATE			9600
 #define RADIO_NUM_DATA_BITS		8
 #define RADIO_DELAY				20  // ms delay between bits
-
-//Defines for what each bit is meant to mean. 
-//TODO check this adheres to the RTTY protocol properly!
-/*
-#define START_BIT				0
-#define STOP_BIT				1
-#define LOGICAL_ONE				100
-#define LOGICAL_ZERO			        183
-*/
+#define RADIO_FREQ_KHZ			434650
 
 //~0.21V swing (~425Hz)
 #define LOGICAL_ONE                              120
@@ -69,6 +48,7 @@
 static void radio_transmit_string(char* string);
 static void _radio_transmit_bit(uint8_t data, uint8_t ptr);
 static uint16_t radio_calculate_checksum(char* data);
+static int mtx2SetFreqTemp(uint32_t freqKHz);
 
 static SerialConfig usartRadio =
 {
@@ -85,8 +65,6 @@ static const DACConfig daccfg = {
 
 /**
 * Take a channel and a value and send it to the radio.
-* TODO Integration with DAC such that we can send any 
-* signal- CHECK THIS
 */
 static void radio_write(uint8_t channel, uint16_t val)
 {
@@ -106,60 +84,15 @@ static void radio_write(uint8_t channel, uint16_t val)
 		                //gpio_set(RADIO_TX_GPIO, RADIO_CHANNEL_MSB);
 		palSetPad(RADIO_TX_GPIO, RADIO_CHANNEL_MSB);
 
-	//Send value to DAC. TODO CHECK THIS WORKS!!!
-	                //dac_software_trigger(CHANNEL_1);
-	                //dac_load_data_buffer_single(val, RIGHT8, CHANNEL_1);
 	dacPutChannelX(&RADIO_DAC, 0, val);
 	
 }
-
-                /*
-                * Initialise the peripheral clocks.
-                */
-                /*
-                static void clock_setup(void)
-                {
-	                //rcc_clock_setup_in_hsi_out_48mhz();  // Necessary?
-	                rcc_periph_clock_enable(RCC_GPIOA);
-
-	                /* Enable clocks for USART1. 
-	                rcc_periph_clock_enable(RCC_USART1);
-
-	                /* Enable clock for timer6, to be used for interrupts 
-	                rcc_periph_clock_enable(RCC_TIM2);
-
-	                //Enable APB1 (Advanced Peripheral Bus) clock for DAC
-	                rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_DACEN);
-                }*/
-
-                /*
-                * Set up GPIOA for USART transmit.
-
-                static void gpio_setup(void)
-                {
-	                gpio_mode_setup(RADIO_EN_GPIO, GPIO_MODE_AF, GPIO_PUPD_NONE, RADIO_EN_PIN);
-
-	                /* Setup USART1 TX pin as alternate function. 
-	                /* GPIO_AF1 is USART1_TX 
-	                gpio_set_af(RADIO_EN_GPIO, GPIO_AF1, RADIO_EN_PIN);
-                }
-                */
 
 /**
 * Configure RADIO_USART (USART1) for Radiometrix MTX2.
 */
 static void usart_setup(void)
-{
-                        /*
-	                usart_set_baudrate(RADIO_USART, RADIO_BAUD_RATE);
-	                usart_set_databits(RADIO_USART, RADIO_NUM_DATA_BITS);
-	                usart_set_stopbits(RADIO_USART, USART_CR2_STOP_1_0BIT);
-	                usart_set_mode(RADIO_USART, USART_MODE_TX);
-	                usart_set_parity(RADIO_USART, USART_PARITY_NONE);
-	                usart_set_flow_control(RADIO_USART, USART_FLOWCONTROL_NONE);
-	                usart_enable(RADIO_USART);
-	                */
-	
+{	
 	sdStart(&SDRV, &usartRadio);
 }
 
@@ -169,96 +102,26 @@ static void usart_setup(void)
 */
 static void dac_setup(void)
 {
-                        /*
-                        gpio_mode_setup(RADIO_TX_GPIO, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, RADIO_TX_DATA);
-                        dac_disable(CHANNEL_1);
-                        dac_set_waveform_characteristics(DAC_CR_MAMP1_8);
-                        dac_set_waveform_generation(DAC_CR_WAVE1_TRI);
-                        dac_enable(CHANNEL_1);
-                        dac_set_trigger_source(DAC_CR_TSEL1_SW);
-                        dac_load_data_buffer_single(0, RIGHT8, CHANNEL_1);
-                        dac_software_trigger(CHANNEL_1);
-                        */
-        palSetPadMode(RADIO_EN_GPIO, RADIO_TX_DATA, PAL_MODE_INPUT_ANALOG);
+    palSetPadMode(RADIO_EN_GPIO, RADIO_TX_DATA, PAL_MODE_INPUT_ANALOG);
 	dacStart(&RADIO_DAC, &daccfg);
 }
-
-                /**
-                * Set up TIM2 for radio interrupts
-                * TODO Check that we don't need to start the timer immediately
-                */
-                /*static void setup_TIM2(void)
-                {
-                        timer_reset(TIM2);
-                        /* 48MHz / 12kHz -1. 
-                        timer_set_prescaler(TIM2, 3999); /* 48MHz/12kHz - 1 
-                        /* 12kHz for 1 tick = 12 khz overflow 
-                        timer_set_period(TIM2, 1);
-                        timer_continuous_mode(TIM2);
-                }*/
-
-/**
-* Enable interrupts on TIM2 and start it.
-* TODO Check that we don't need to reset the timer
-*/
-/*static void start_TIM2(void)
-{
-	timer_disable_counter(TIM2);
-	timer_clear_flag(TIM2, TIM_SR_UIF);
-	nvic_enable_irq(NVIC_TIM2_IRQ);
-	timer_enable_update_event(TIM2); /* Unclear 
-	timer_enable_irq(TIM2, TIM_DIER_UIE);
-	timer_set_counter(TIM2, 0);
-	timer_enable_counter(TIM2);
-}*/
-
-/**
-* Disable interrupts on TIM2 and stop it
-* TODO Check that we don't need to reset the timer
-*/
-/*
-static void stop_TIM2(void)
-{
-	timer_clear_flag(TIM2, TIM_SR_UIF);
-	nvic_disable_irq(NVIC_TIM2_IRQ);
-	timer_disable_counter(TIM2);
-	timer_set_counter(TIM2, 0);
-}*/
 
 /**
  * Initialise the radio module and all the timers required.
  */
 void radio_init(void)
 {
-	//clock_setup();
-	//gpio_setup();
 	usart_setup();
-	//setup_TIM2();
 	dac_setup();
 	
-	//!!!!!
+	
+	mtx2SetFreqTemp(RADIO_FREQ_KHZ);
+	
 	//TODO add other initialisation stuff here, like setting
 	//the operating frequency, channel number, and TX power.
 	//See datasheet for more info on the command strings.
-    
-    //radio_set_baud(RADIO_BAUD_50); RTTY NOT SERIAL
 
 }
-
-/*static THD_FUNCTION(radio_transmit_string, arg){
-	char* string = (char* )arg;
-	
-	while(*string){
-		//Transmit byte
-		//Send 11 bits (1 start, 8 data, 2 stop)
-		for(int _txptr = 0; _txptr < 10; _txptr++){
-			_radio_transmit_bit(*string, _txptr);
-			chThdSleepMilliseconds(20); // 50 baud
-		}
-		string++;
-	}
-	chthdExit(0);
-}*/
 
 static void radio_transmit_string(char* string){
 	while(*string){
@@ -270,8 +133,10 @@ static void radio_transmit_string(char* string){
 			_radio_transmit_bit(*string, _txptr);
 			
 			while(chVTIsSystemTimeWithin(startTime, endTime)){
+				//iwdg_reset();
 			}
 		}
+		//iwdg_reset();
 		string++;
 	}
 }
@@ -290,49 +155,7 @@ void radio_transmit_sentence(char * message)
     strcpy(fullmsg, message);
     strcat(fullmsg, cs);
     radio_transmit_string(fullmsg);
-    //thread_t *tx = chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(128),NORMALPRIO, radio_transmit_string, (void *)fullmsg);
-    //thread_t *tx = chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(128), NORMALPRIO, radio_transmit_string, (void *)fullmsg);
-    /*if (tp == NULL)
-		chSysHalt("out of memory");*/
-	//msg_t msg = chThdWait(tx);  // Return thread memory to heap
 }
-
-/*static THD_FUNCTION(_radio_transmit_byte, arg){
-	uint8_t _txbyte = (uint8_t)arg;
-	
-	//Send 11 bits (1 start, 8 data, 2 stop)
-	for(int _txptr = 0; _txptr < 10; _txptr++){
-		_radio_transmit_bit(_txbyte, _txptr);
-		chThdSleepMilliseconds(20); // 50 baud
-	}
-	
-	chthdExit(0);
-}*/
-
-
-/*static void radio_transmit_string(char* string)
-{
-	/*while(*string)
-    {
-        _txbyte = *string;
-        _txptr = 0;
-        byte_complete = false;
-        // Enable interrupts
-        start_TIM2();
-        while(!byte_complete)
-        	;
-        //iwdg_reset();
-        string++;
-    }*//*
-    while(*string){
-		thread_t *tx = chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(128),
-		                                   NORMALPRIO, _radio_transmit_byte, *string);
-		/*if (tp == NULL)
-			chSysHalt("out of memory");*//*
-		msg_t msg = chThdWait(tx);  // Return thread memory to heap
-		string++;
-	}
-}*/
 
 /**
 * Some radio chatter so that Woodchuck can be located
@@ -347,6 +170,7 @@ void radio_chatter(void)
 	chThdSleepMilliseconds(200);
 	radio_write(0,200);
 	chThdSleepMilliseconds(200);
+	//iwdg_reset();  // Necessary?
 }
 
 /**
@@ -368,7 +192,7 @@ static uint16_t radio_calculate_checksum(char* data)
 				if(crc & 0x8000)
 					crc = (crc << 1) ^ 0x1021;
 				else
-					crc << 1;
+					crc <<= 1;
 			}
 		}
     }
@@ -397,4 +221,20 @@ static void _radio_transmit_bit(uint8_t data, uint8_t ptr)
     	radio_write(0, STOP_BIT);
     	radio_write(0, STOP_BIT);
     }
+}
+
+static int mtx2SetFreqTemp(uint32_t freqKHz){
+	uint8_t integer;
+	uint64_t fraction;
+	integer = 0xFF & freqKHz/6500;  // Round down
+	integer--;
+	fraction = (uint64_t)freqKHz * 0x80000 / 6500;
+	fraction -= (uint64_t)integer * 0x80000;
+	if(fraction < 0x80000 || fraction > 0xFFFFF){
+		return 1;  // Fraction should not lie outside these values
+	}
+	char command[14];
+	snprintf(command, sizeof(command), "@PRG_%02X%06llX\r", integer, fraction);
+	sdWrite(&SDRV, (uint8_t *) command, strlen(command));
+	return 0;  // Success
 }
